9c4da6581a2bca8f7b145096d285316118ea6915
aakshayb <aakshayb@gmail.com>
2016-04-03 22:57:34 -0400

    Few updates

--- src/com/homeworks/Greedy/Solution.java
+++ src/com/homeworks/Greedy/Solution.java
@@ -3,6 +3,7 @@ package com.homeworks.Greedy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Akshay on 4/3/2016.
@@ -14,6 +15,7 @@ public class Solution
     Solution solution = new Solution();
     int maxProduct = solution.maxp3(new ArrayList<>(Arrays.asList(3, -5, 20)));
     int maxDistance = solution.mice(new ArrayList<>(Arrays.asList(4, -4, 2)), new ArrayList<>(Arrays.asList(4,0,5)));
+    int canComplete = solution.mice(new ArrayList<>(Arrays.asList(1,2)), new ArrayList<>(Arrays.asList(2,1)));
     int ret =0;
   }
   public int maxp3(ArrayList<Integer> a) {
@@ -36,4 +38,26 @@ public class Solution
       maxDistance = Math.max(maxDistance, Math.abs(a.get(i) - b.get(i)) );
     return maxDistance;
   }
+
+  public int canCompleteCircuit(final List<Integer> gas, final List<Integer> cost) {
+    int start = 0;
+    int end =1;
+    if(gas.size() <= 1)
+      return 0;
+    int currCost = gas.get(start) - cost.get(start);
+
+    while(end!=start || currCost < 0)
+    {
+      while(currCost <0 && start!=end)
+      {
+        currCost -= gas.get(start) - cost.get(start);
+        start = (start+1)%gas.size();
+        if(start ==0)return -1;
+      }
+      currCost += gas.get(end) - cost.get(end);
+      end = (end+1)%gas.size();
+    }
+    return start;
+  }
+
 }
